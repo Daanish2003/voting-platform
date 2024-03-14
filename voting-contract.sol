2fe72a582373e329d689b7ffe44dccf3fd32a7ad
Daanish Azim Sheikh <redchillies6786@gmail.com>
2024-03-14 14:52:31 +0530

    candidates function are added

--- voting-contract.sol
+++ voting-contract.sol
@@ -3,15 +3,56 @@ pragma solidity ^0.8.0;
 
 // contract for voting system
 contract Ballot {
-    // unsigned integer for store the vote value
-    // initial voting value to 0
-    uint public votes = 0;
-   // set voting value
-   function setVotes() public {
-    votes = votes + 1;
-   }
-   // get voting value
-   function getVotes() public view returns(uint) {
-    return votes;
-   }
+
+    // Keep the tract of number of candidate and fullfill the function to generate unique id
+    uint public candidateCount;
+
+
+    // constructor for adding candidate
+    constructor () {
+      addCandidate("Ken");
+      addCandidate("Ben");
+    } 
+
+    // structure of the canditate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //mapping to number of candidates
+    // mapping to the voter address
+    mapping (uint => Candidate) public candidateLookup;
+    mapping(address => bool) public voterLookup;
+
+
+    // function to add canditate
+    function addCandidate(string memory name) public {
+        candidateLookup[candidateCount] = Candidate(candidateCount, name, 0);
+        candidateCount ++;
+    }
+
+    // function get all candidate at once
+    function getCandidate() external view returns(string[] memory, uint[] memory) {
+        string[] memory names = new string[] (candidateCount);
+        uint[] memory voteCounts = new uint[](candidateCount);
+        for (uint i = 0; i < candidateCount; i++) {
+        names[i] = candidateLookup[i].name;
+        voteCounts[i] = candidateLookup[i].voteCount;
+    }
+    return (names, voteCounts);
+    }
+
+    // voting function
+    // checks for the voter id
+    function vote(uint id) external {
+        require (!voterLookup[msg.sender]);
+        require (id >= 0 && id <= candidateCount-1);
+        candidateLookup[id].voteCount++;
+        emit votedEvent(id);
+}
+
+event votedEvent(uint indexed id);
+
 }
